72cd5af20d40cf41fc3b1a0e8ea8789701dd7433
ACS107104 <acs107104@gm.ntcu.edu.tw>
2022-06-25 17:25:07 +0800

    hw4

---
+++ controllers/web_controller.go
@@ -0,0 +1,201 @@
+/*
+Copyright 2022.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+
+	appv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	hw4v1alpha1 "github.com/ogre0403/110-2-ntcu-k8s-programing/api/v1alpha1"
+)
+
+// WebReconciler reconciles a Web object
+type WebReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+//+kubebuilder:rbac:groups=hw4.ntcu.edu.tw,resources=webs,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=hw4.ntcu.edu.tw,resources=webs/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=hw4.ntcu.edu.tw,resources=webs/finalizers,verbs=update
+
+//+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create;update;patch;delete
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the Web object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.12.1/pkg/reconcile
+func (r *WebReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	log := log.FromContext(ctx)
+
+	// TODO(user): your logic here
+	web := &hw4v1alpha1.Web{}
+	err := r.Get(ctx, req.NamespacedName, web)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// Request object not found, could have been deleted after reconcile request.
+			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
+			// Return and don't requeue
+			log.Info("Web resource not found. Ignoring since object must be deleted")
+			return ctrl.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		log.Error(err, "Failed to get web")
+		return ctrl.Result{}, err
+	}
+
+	foundDeployment := &appv1.Deployment{}
+	err = r.Get(ctx, types.NamespacedName{Name: web.Name, Namespace: web.Namespace}, foundDeployment)
+	if err != nil && errors.IsNotFound(err) {
+		// Define a new deployment
+		dep := r.newDeployment(web)
+		log.Info("Creating a new Deployment", "Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
+		err = r.Create(ctx, dep)
+		if err != nil {
+			log.Error(err, "Failed to create new Deployment", "Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
+			return ctrl.Result{}, err
+		}
+		// Deployment created successfully - return and requeue
+		return ctrl.Result{Requeue: true}, nil
+	} else if err != nil {
+		log.Error(err, "Failed to get Deployment")
+		return ctrl.Result{}, err
+	}
+
+	foundService := &corev1.Service{}
+	err = r.Get(ctx, types.NamespacedName{Name: web.Name, Namespace: web.Namespace}, foundService)
+	if err != nil && errors.IsNotFound(err) {
+		// Define a new deployment
+		svc := r.newService(web)
+		log.Info("Creating a new Service", "Service.Namespace", svc.Namespace, "Service.Name", svc.Name)
+		err = r.Create(ctx, svc)
+		if err != nil {
+			log.Error(err, "Failed to create new Service", "Service.Namespace", svc.Namespace, "ConfigMap.Name", svc.Name)
+			return ctrl.Result{}, err
+		}
+		// Service created successfully - return and requeue
+		return ctrl.Result{Requeue: true}, nil
+	} else if err != nil {
+		log.Error(err, "Failed to get ConfigMap")
+		return ctrl.Result{}, err
+	}
+
+	return ctrl.Result{}, nil
+}
+
+func int32Ptr(i int32) *int32 { return &i }
+
+func (r *WebReconciler) newDeployment(s *hw4v1alpha1.Web) *appv1.Deployment {
+	dm := &appv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: s.Name,
+			Labels: map[string]string{
+				"ntcu-k8s": "hw4",
+			},
+		},
+		Spec: appv1.DeploymentSpec{
+			Replicas: int32Ptr(1),
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"ntcu-k8s": "hw4",
+				},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"ntcu-k8s": "hw4",
+					},
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  s.Name,
+							Image: s.Spec.Image,
+							Ports: []corev1.ContainerPort{
+								{
+									Name:          "http",
+									ContainerPort: 80,
+									Protocol:      corev1.ProtocolTCP,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	dm.Name = s.Name
+	dm.Namespace = s.Namespace
+
+	ctrl.SetControllerReference(s, dm, r.Scheme)
+	return dm
+}
+
+var portnum int32 = 80
+
+func (r *WebReconciler) newService(s *hw4v1alpha1.Web) *corev1.Service {
+	sm := &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: s.Name,
+			Labels: map[string]string{
+				"ntcu-k8s": "hw4",
+			},
+		},
+		Spec: corev1.ServiceSpec{
+			Selector: map[string]string{
+				"ntcu-k8s": "hw4",
+			},
+			Type: corev1.ServiceTypeNodePort,
+			Ports: []corev1.ServicePort{
+				{
+					Name:       "http",
+					Port:       80,
+					TargetPort: intstr.IntOrString{IntVal: portnum},
+					NodePort:   s.Spec.NodePortNumber,
+					Protocol:   corev1.ProtocolTCP,
+				},
+			},
+		},
+	}
+	sm.Name = s.Name
+	sm.Namespace = s.Namespace
+
+	ctrl.SetControllerReference(s, sm, r.Scheme)
+	return sm
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *WebReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&hw4v1alpha1.Web{}).
+		Owns(&appv1.Deployment{}).
+		Owns(&corev1.Service{}).
+		Complete(r)
+}
